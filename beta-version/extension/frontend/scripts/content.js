a719b8c6686818546325241106116f631cd90c15
Brian Ruff <briananthonyruff@gmail.com>
2023-11-13 21:00:21 -0500

    added new prettierrc yaml file to make up for removing eslint for formatting purposes

--- beta-version/extension/frontend/scripts/content.js
+++ beta-version/extension/frontend/scripts/content.js
@@ -1,147 +1,147 @@
 const state = {
-  shouldCleanUp: false,
-  gpt4Button: null,
+	shouldCleanUp: false,
+	gpt4Button: null,
 };
 
-chrome.storage.local.get("selectedText", (data) => main(data));
+chrome.storage.local.get('selectedText', (data) => main(data));
 
 const main = async (data) => {
-  if (data.selectedText) {
-    // timeout is to ensure the textarea has loaded
-    setTimeout(() => {
-      inputSelectedText(data);
-    }, 1000);
+	if (data.selectedText) {
+		// timeout is to ensure the textarea has loaded
+		setTimeout(() => {
+			inputSelectedText(data);
+		}, 1000);
 
-    // await clickChatGPT4Button();
+		// await clickChatGPT4Button();
 
-    // timeout is to ensure the submit button is clickable
-    setTimeout(() => {
-      submitQuestionToChatGPT();
-    }, 1000);
+		// timeout is to ensure the submit button is clickable
+		setTimeout(() => {
+			submitQuestionToChatGPT();
+		}, 1000);
 
-    chrome.storage.local.remove("selectedText");
-  }
+		chrome.storage.local.remove('selectedText');
+	}
 };
 
 const inputSelectedText = (data) => {
-  const textArea = document.querySelector("#prompt-textarea");
+	const textArea = document.querySelector('#prompt-textarea');
 
-  if (textArea) {
-    const leadInText = getRandomLeadInText();
+	if (textArea) {
+		const leadInText = getRandomLeadInText();
 
-    const textToInsert = `${leadInText} "${data.selectedText}"`;
+		const textToInsert = `${leadInText} "${data.selectedText}"`;
 
-    textArea.value = textToInsert;
+		textArea.value = textToInsert;
 
-    // simulates typing to enable the submit button
-    const inputEvent = new Event("input", {
-      bubbles: true,
-      cancelable: true,
-    });
+		// simulates typing to enable the submit button
+		const inputEvent = new Event('input', {
+			bubbles: true,
+			cancelable: true,
+		});
 
-    textArea.dispatchEvent(inputEvent);
-  }
+		textArea.dispatchEvent(inputEvent);
+	}
 };
 
 const _waitForChatGPT4Button = async () => {
-  const loadingModalPortal = _createLoadingModalPortal();
+	const loadingModalPortal = _createLoadingModalPortal();
 
-  return new Promise((resolve, reject) => {
-    const timeoutMillis = 10000;
+	return new Promise((resolve, reject) => {
+		const timeoutMillis = 10000;
 
-    const startTime = Date.now();
+		const startTime = Date.now();
 
-    const interval = setInterval(() => {
-      const chatGPT4Button = document.querySelector(
-        '[data-testid="gpt-4"]'
-      )?.firstChild;
+		const interval = setInterval(() => {
+			const chatGPT4Button = document.querySelector(
+				'[data-testid="gpt-4"]',
+			)?.firstChild;
 
-      // button found successfully
-      if (chatGPT4Button && !chatGPT4Button.disabled) {
-        clearInterval(interval);
+			// button found successfully
+			if (chatGPT4Button && !chatGPT4Button.disabled) {
+				clearInterval(interval);
 
-        if (loadingModalPortal) {
-          loadingModalPortal.remove();
-        }
+				if (loadingModalPortal) {
+					loadingModalPortal.remove();
+				}
 
-        state.gpt4Button = chatGPT4Button;
+				state.gpt4Button = chatGPT4Button;
 
-        resolve();
-      }
+				resolve();
+			}
 
-      const regenerateButton = [...document.querySelectorAll("div")].filter(
-        (svg) => svg.innerText === "Regenerate"
-      )[1];
+			const regenerateButton = [...document.querySelectorAll('div')].filter(
+				(svg) => svg.innerText === 'Regenerate',
+			)[1];
 
-      // This is the "unusual activity" message workaround
-      if (regenerateButton) {
-        if (loadingModalPortal) {
-          loadingModalPortal.remove();
-        }
+			// This is the "unusual activity" message workaround
+			if (regenerateButton) {
+				if (loadingModalPortal) {
+					loadingModalPortal.remove();
+				}
 
-        // wait for the button to be clickable
-        setTimeout(() => {
-          regenerateButton.click();
+				// wait for the button to be clickable
+				setTimeout(() => {
+					regenerateButton.click();
 
-          clearInterval(interval);
-        }, 1000);
-      }
+					clearInterval(interval);
+				}, 1000);
+			}
 
-      const elapsedTime = Date.now() - startTime;
+			const elapsedTime = Date.now() - startTime;
 
-      // 5 seconds have passed or something else went wrong, either way, we need to stop trying to check for the button
-      if (elapsedTime >= timeoutMillis) {
-        if (loadingModalPortal) {
-          loadingModalPortal.remove();
-        }
+			// 5 seconds have passed or something else went wrong, either way, we need to stop trying to check for the button
+			if (elapsedTime >= timeoutMillis) {
+				if (loadingModalPortal) {
+					loadingModalPortal.remove();
+				}
 
-        clearInterval(interval);
+				clearInterval(interval);
 
-        reject(
-          new Error(
-            `It is possible your network is having problems loading the page within ${timeoutMillis} milliseconds if you are seeing this alert. In which case, you will need to either try again later once your network is more stable, or are on a network with a faster coonection.`
-          )
-        );
-      }
-    }, 100);
-  });
+				reject(
+					new Error(
+						`It is possible your network is having problems loading the page within ${timeoutMillis} milliseconds if you are seeing this alert. In which case, you will need to either try again later once your network is more stable, or are on a network with a faster coonection.`,
+					),
+				);
+			}
+		}, 100);
+	});
 };
 
 const _clickChatGPT4Button = async () => {
-  try {
-    await _waitForChatGPT4Button();
+	try {
+		await _waitForChatGPT4Button();
 
-    if (state.gpt4Button && !state.gpt4Button.disabled) {
-      state.gpt4Button.click();
-    }
-  } catch (error) {
-    alert(error);
-  }
+		if (state.gpt4Button && !state.gpt4Button.disabled) {
+			state.gpt4Button.click();
+		}
+	} catch (error) {
+		alert(error);
+	}
 };
 
 const submitQuestionToChatGPT = () => {
-  const sendButton = document.querySelector('[data-testid="send-button"]');
+	const sendButton = document.querySelector('[data-testid="send-button"]');
 
-  if (sendButton && !sendButton.disabled) {
-    sendButton.click();
+	if (sendButton && !sendButton.disabled) {
+		sendButton.click();
 
-    state.shouldCleanUp = true;
-  }
+		state.shouldCleanUp = true;
+	}
 };
 
 const _createLoadingModalPortal = () => {
-  const existingLoadingModalPortal = document.querySelector(
-    "#loadingModalPortal"
-  );
+	const existingLoadingModalPortal = document.querySelector(
+		'#loadingModalPortal',
+	);
 
-  if (existingLoadingModalPortal) {
-    return existingLoadingModalPortal;
-  } else {
-    const loadingModalPortal = document.createElement("div");
+	if (existingLoadingModalPortal) {
+		return existingLoadingModalPortal;
+	} else {
+		const loadingModalPortal = document.createElement('div');
 
-    loadingModalPortal.id = "loadingModalPortal";
+		loadingModalPortal.id = 'loadingModalPortal';
 
-    loadingModalPortal.innerHTML = `
+		loadingModalPortal.innerHTML = `
 			<div class="loading-modal">
 				<div class="loading-modal-color-layer">
 					<div class="progress-bar">
@@ -154,55 +154,55 @@ const _createLoadingModalPortal = () => {
 			</div>
 		`;
 
-    document.body.insertAdjacentElement("afterEnd", loadingModalPortal);
+		document.body.insertAdjacentElement('afterEnd', loadingModalPortal);
 
-    return loadingModalPortal;
-  }
+		return loadingModalPortal;
+	}
 };
 
 const getRandomLeadInText = () => {
-  const leadInList = [
-    "Explain:",
-    "Clarify:",
-    "Elucidate:",
-    "Analyze:",
-    "Define:",
-    "Explicate:",
-    "Describe:",
-    "Expound on:",
-    "What does this mean?:",
-    "Interpret:",
-    "Break down:",
-    "Unpack:",
-    "Delineate:",
-    "Give insight on:",
-    "Shed light on:",
-    "Detail:",
-    "Outline:",
-    "Illustrate:",
-    "Provide an overview of:",
-    "What's your take on:",
-    "Can you detail:",
-    "Discuss:",
-    "Examine:",
-    "Comment on:",
-    "Decipher:",
-    "Convey:",
-    "Articulate:",
-    "Enlighten us on:",
-    "Demystify:",
-    "Provide clarity on:",
-    "Can you elaborate on:",
-    "Give an explanation of:",
-    "Help understand:",
-    "What is the significance of:",
-    "Provide context for:",
-    "Can you interpret:",
-    "Talk about:",
-    "Unravel:",
-  ];
+	const leadInList = [
+		'Explain:',
+		'Clarify:',
+		'Elucidate:',
+		'Analyze:',
+		'Define:',
+		'Explicate:',
+		'Describe:',
+		'Expound on:',
+		'What does this mean?:',
+		'Interpret:',
+		'Break down:',
+		'Unpack:',
+		'Delineate:',
+		'Give insight on:',
+		'Shed light on:',
+		'Detail:',
+		'Outline:',
+		'Illustrate:',
+		'Provide an overview of:',
+		"What's your take on:",
+		'Can you detail:',
+		'Discuss:',
+		'Examine:',
+		'Comment on:',
+		'Decipher:',
+		'Convey:',
+		'Articulate:',
+		'Enlighten us on:',
+		'Demystify:',
+		'Provide clarity on:',
+		'Can you elaborate on:',
+		'Give an explanation of:',
+		'Help understand:',
+		'What is the significance of:',
+		'Provide context for:',
+		'Can you interpret:',
+		'Talk about:',
+		'Unravel:',
+	];
 
-  const randomIndex = Math.floor(Math.random() * leadInList.length);
+	const randomIndex = Math.floor(Math.random() * leadInList.length);
 
-  return leadInList[randomIndex];
+	return leadInList[randomIndex];
 };
